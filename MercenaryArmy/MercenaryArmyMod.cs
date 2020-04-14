3e7eef3dee8cace2402f12e5f9847b528f005478
Splintert <splintert@ymail.com>
2020-04-13 22:36:32 -0500

    Add project files.

---
+++ MercenaryArmy/MercenaryArmyMod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.Library;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+
+namespace MercenaryArmy
+{
+    [HarmonyPatch(typeof(MapVM), "CanGatherArmyWithReason")]
+    public class MapVMMod
+    {
+        static TextObject err1;
+        static TextObject err2;
+
+        static void Postfix(ref bool __result, ref string reasonText)
+        {
+            if (err1 == null || err2 == null)
+            {
+                err1 = GameTexts.FindText("str_need_to_be_a_part_of_kingdom", (string)null);
+                err2 = GameTexts.FindText("str_mercenary_cannot_manage_army", (string)null);
+            }
+            if (__result == false && new[] { err1.ToString(), err2.ToString() }.Contains(reasonText))
+            {
+                __result = true;
+                reasonText = String.Empty;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ArmyManagementVM), MethodType.Constructor, new Type[] { typeof(Action) })]
+    public class ArmyManagementVMMod
+    {
+        static void Postfix(ArmyManagementVM __instance)
+        {
+            if (!Hero.MainHero.MapFaction.IsKingdomFaction || Clan.PlayerClan.IsUnderMercenaryService)
+            {
+                for (int x = 0; x < __instance.PartyList.Count; x++)
+                {
+                    if (__instance.PartyList[x].Clan.Id != MobileParty.MainParty.LeaderHero.Clan.Id)
+                    {
+                        __instance.PartyList.RemoveAt(x);
+                        x--;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MercenaryArmy/SubModule.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Windows.Forms;
+using TaleWorlds.MountAndBlade;
+
+namespace MercenaryArmy
+{
+    class SubModule : MBSubModuleBase
+    {
+        public static readonly string ModuleFolderName = "MercenaryArmy";
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            try
+            {
+                var harmony = new Harmony("mod.bannerlord.splintert");
+                harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error Initialising MercenaryArmy:\n\n{ex.Message}");
+            }
+        }
+    }
+}
